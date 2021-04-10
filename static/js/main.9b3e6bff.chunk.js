1d1eb11a6197c075b639bdbe029fd21cf864bcc3
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-04-10 01:47:53 -0700

    Updates

---
+++ static/js/main.9b3e6bff.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppassa-senate"]=this["webpackJsonppassa-senate"]||[]).push([[0],{20:function(e,t,s){},21:function(e,t,s){},31:function(e,t,s){"use strict";s.r(t);s(0);var c=s(14),a=s.n(c),n=(s(20),s(11)),j=s(1),r=(s(21),s(6));var i=function(){return Object(r.jsxs)("div",{className:"App",children:[Object(r.jsx)(j.a,{exact:!1,path:"/"}),Object(r.jsx)(j.c,{children:Object(r.jsx)(j.a,{exact:!0,path:"/"})})]})};a.a.render(Object(r.jsx)(n.a,{children:Object(r.jsx)(i,{})}),document.getElementById("root"))}},[[31,1,2]]]);
+//# sourceMappingURL=main.9b3e6bff.chunk.js.map
